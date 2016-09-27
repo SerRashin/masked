9e1bfaf44a7f7b5972c815aff52efacae4e94a2a
serhanters <serhanters@yandex.ru>
2016-09-27 11:38:21 +0300

    новые gh-pages

--- dev/js/src/config.js
+++ dev/js/src/config.js
@@ -10,7 +10,7 @@ var MaskedConfig = MConf = (function() {
     };
 
     var options = {
-        pathToList:         '/js/masks/',
+        pathToList:         '//serhanters.github.io/masked/js/masks/',
         prefix:             'instId_',
         lang:               'ru',
         country:            'ru',
@@ -21,7 +21,7 @@ var MaskedConfig = MConf = (function() {
         select_range:       false
     };
 
-    
+
     return function (args) {
         if (typeof args === 'string') {
             return options[args];
